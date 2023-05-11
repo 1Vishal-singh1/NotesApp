115d1573ccbd55fa81665c0da1bad19a554e8f19
Singh <vishal.q.singh@capgemini.com>
2023-05-11 12:20:38 +0530

    first commit

---
+++ src/components/MyComponent.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+
+const MyComponent  = () => {
+    return (
+        <div>
+            <Button variant="contained" color="primary">
+      Hello World
+    </Button>
+        </div>
+    );
+};
+
+export default MyComponent ;
